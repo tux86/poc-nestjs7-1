c04eab2feb76f5988e092e526d7d294adadf1f0b
walid <walid.karray@gmail.com>
2022-04-03 23:52:34 +0200

    course 11

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,63 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post, Session, UseGuards, UseInterceptors} from '@nestjs/common';
 import {CreateUserDto} from "./dtos/create-user.dto";
 import {UsersService} from "./users.service";
 import {UpdateUserDto} from "./dtos/update-user.dto";
 import {Serialize} from "../interceptors/serialize.interceptor";
 import {UserDto} from "./dtos/user.dto";
+import {AuthService} from "./auth.service";
+import {User} from "./user.entity";
+import {CurrentUser} from "./decorators/current-user.decorator";
+import {AuthGuard} from "../guards/auth.guard";
 
 @Controller('auth')
 @Serialize(UserDto)
+
 export class UsersController {
 
-    constructor(private usersService: UsersService) {
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService
+    ) {
     }
 
+
+    @Get('/colors/:color')
+    setColor(@Param('color') color: string, @Session() session: any) {
+        session.color = color
+    }
+
+
+    @Get('/colors')
+    getColor(@Session() session: any) {
+        return session.color
+    }
+
+
+    @Get('/whoami')
+    @UseGuards(AuthGuard)
+    whoAmI(@CurrentUser() user: User) {
+        return user
+    }
+
+
     @Post('/signup')
-    async createUser(@Body() body: CreateUserDto) {
-        await this.usersService.create(body.email, body.password)
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password)
+        session.userId = user.id
+        return user
+    }
+
+
+    @Post('/signin')
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password)
+        session.userId = user.id
+        return user
+    }
+
+    @Post('/signout')
+    async signout(@Session() session: any) {
+        session.userId = null
     }
 
     @Get('/:id')
